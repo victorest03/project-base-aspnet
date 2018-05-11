b42fc382bf53264d0ed63143888ae8c191969209
Victor Manuel Estrada Llancary <elvictor-003@hotmail.com>
2018-05-11 00:19:39 -0500

    add IRepository

--- BaseProject.Model/Usuario.cs
+++ BaseProject.Model/Usuario.cs
@@ -1,9 +1,21 @@
-﻿namespace BaseProject.Model
+﻿using FluentValidation;
+
+namespace BaseProject.Model
 {
     public class Usuario
     {
         public int pkUsuario { get; set; }
         public string cUsuario { get; set; }
         public string cPassword { get; set; }
+    }
+
+    public class UsuarioValidator : AbstractValidator<Usuario>
+    {
+        public UsuarioValidator()
+        {
+            RuleFor(usuario => usuario.cUsuario).NotEmpty().WithMessage("Ingresar un Nombre de Usuario!!!");
+            RuleFor(usuario => usuario.cPassword).NotEmpty().WithMessage("Ingresar una Contraseña!!!");
+
+        }
     }
 }
